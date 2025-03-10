3b5638bbec2fa1f957ceb8ebadea301c978e642b
KlausBast27 <kbdeutz@web.de>
2025-03-10 17:01:07 +0100

    Add files via upload

---
+++ learn2code_exam_Kai.Retig_Klaus.Bast/code_1_draft/include/developer.h
@@ -0,0 +1,6 @@
+#ifndef DEVELOPER_LIB_H
+#define DEVELOPER_LIB_H
+
+void print_developer();
+
+#endif
---
+++ learn2code_exam_Kai.Retig_Klaus.Bast/code_1_draft/src/developer.c
@@ -0,0 +1,5 @@
+#include "developer.h"
+#include <stdio.h>
+void print_developer(){
+    printf("Kai Retig\nKlaus Bast\n");
+}
